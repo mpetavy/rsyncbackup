c7e62ab1d8e55b86481c630b31f37251ea86ac17
mpetavy <mpetavy>
2025-01-15 01:02:08 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"embed"
+	"flag"
+	"fmt"
+	"github.com/mpetavy/common"
+	"github.com/pkg/errors"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+//go:embed go.mod
+var resources embed.FS
+
+var (
+	sourcePath = flag.String("s", "", "source directory")
+	destPath   = flag.String("d", "", "destination directory")
+	countGen   = flag.Int("c", 3, "number of backup generations")
+	dry        = flag.Bool("n", false, "dry run")
+)
+
+func init() {
+	common.Init("", "", "", "", "", "", "", "", &resources, nil, nil, run, 0)
+}
+
+func checkRsync() (string, error) {
+	rsyncPath, err := exec.LookPath("rsync")
+	if common.Error(err) {
+		return "", errors.Wrap(err, "Please install 'rsync'")
+	}
+
+	return rsyncPath, nil
+}
+
+func checkPath(path string) error {
+	if !common.FileExists(path) || !common.IsDirectory(path) {
+		return &common.ErrFileNotFound{path}
+	}
+
+	return nil
+}
+
+func backupName(gen int) string {
+	return fmt.Sprintf("%s_backup_%03d", filepath.Base(*sourcePath), gen)
+}
+
+func run() error {
+	rsyncPath, err := checkRsync()
+	if common.Error(err) {
+		return err
+	}
+
+	err = checkPath(*sourcePath)
+	if common.Error(err) {
+		return err
+	}
+
+	err = checkPath(*destPath)
+	if common.Error(err) {
+		return err
+	}
+
+	if strings.HasSuffix(*sourcePath, string(os.PathSeparator)) || strings.HasSuffix(*sourcePath, "/") {
+		*sourcePath = (*sourcePath)[:len(*sourcePath)-1]
+	}
+
+	if strings.HasSuffix(*destPath, string(os.PathSeparator)) || strings.HasSuffix(*destPath, "/") {
+		*destPath = (*destPath)[:len(*destPath)-1]
+	}
+
+	for i := *countGen; ; i++ {
+		path := filepath.Join(*destPath, backupName(i))
+		if !common.FileExists(path) {
+			break
+		}
+
+		common.Info("Remove obsolete path: %s", path)
+
+		if !*dry {
+			err := os.RemoveAll(path)
+			if common.Error(err) {
+				return err
+			}
+		}
+	}
+
+	for i := *countGen - 1; i >= 1; i-- {
+		oldPath := filepath.Join(*destPath, backupName(i))
+		newPath := filepath.Join(*destPath, backupName(i+1))
+
+		if !common.FileExists(oldPath) {
+			continue
+		}
+
+		common.Info("Move %s to %s", oldPath, newPath)
+
+		if !*dry {
+			err := os.Rename(oldPath, newPath)
+			if common.Error(err) {
+				return err
+			}
+		}
+	}
+
+	start := time.Now()
+
+	link := fmt.Sprintf("%s%s", filepath.Join(*destPath, backupName(2)), string(os.PathSeparator))
+	source := fmt.Sprintf("%s%s", *sourcePath, string(os.PathSeparator))
+	dest := filepath.Join(*destPath, backupName(1))
+
+	list := []string{}
+
+	list = append(list, "--archive", "--safe-links", "--delete")
+
+	if common.FileExists(link) {
+		list = append(list, fmt.Sprintf("--link-dest=%s", link))
+	}
+
+	list = append(list, source, dest)
+
+	cmd := exec.Command(rsyncPath, list...)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	common.Info(common.CmdToString(cmd))
+
+	err = cmd.Run()
+
+	common.Info("time needed: %v", time.Since(start))
+
+	if common.Error(err) {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	common.Run([]string{"s", "d"})
+}
